d1ca1749b00e9afbb64d43f42482a5d235d7ec16
Narek Tovmasyan <96180347+NarekTovmasyan@users.noreply.github.com>
2022-04-19 22:14:39 +0400

    add get method for user, is user exists

--- src/main/java/com/example/fashionshop/controller/UserController.java
+++ src/main/java/com/example/fashionshop/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.example.fashionshop.controller;
 
+import com.example.fashionshop.model.Order;
+import com.example.fashionshop.model.Product;
 import com.example.fashionshop.model.User;
+import com.example.fashionshop.model.dto.responseDto.ResponseDto;
 import com.example.fashionshop.repository.UserRepository;
+import com.example.fashionshop.service.UserService;
+import com.example.fashionshop.validation.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +20,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     /***
      *
@@ -21,6 +30,13 @@ public class UserController {
      */
     @GetMapping
     List<User> getAll(){
-        return userRepository.findAll();
+        return userService.getAll();
+    }
+
+    @GetMapping("user-id")
+    ResponseEntity<ResponseDto> isUserExists(@RequestHeader String userId) {
+        ResponseDto responseDto = new ResponseDto("user exists");
+        responseDto.addInfo("exists", String.valueOf(userService.isExists(userId)));
+        return ResponseEntity.ok(responseDto);
     }
 }
